02144248b5b9eb1437eb145402e2604b7e7a039b
towry <tovvry@gmail.com>
2016-08-30 16:52:02 +0800

    Add pseudo code to insert

--- sort/insert.cpp
+++ sort/insert.cpp
@@ -1,6 +1,22 @@
 // Copyright 2015 Towry Wang (http://towry.me)
 // MIT Licensed
 
+/*
+
+Set i = 1
+Set arr[]
+While i < the length of arr
+  i += 1
+  // current i is the unsorted one.
+  Set j = last index of sorted part
+  While j >= 0
+    j -= 1
+    If current unsorted one is less than it's former
+      swap the position of current unsorted one with it's former
+    Else
+      break
+*/
+
 #include "../include/sort.hpp"
 
 namespace sort {
